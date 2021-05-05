4cdd7318e7b9951fe6ed45a9274736251eb9aa9c
Alexander Park <parkalexser@gmail.com>
2021-05-05 17:21:57 +0500

    userGetInfo

--- routes/botman.php
+++ routes/botman.php
@@ -7,7 +7,7 @@ $botman->hears('/random', 'App\Http\Controllers\AllBreedsController@random');
 
 $botman->hears('Hi', function ($bot) {
 	$user = $bot->getUser();
-    $bot->reply(print_r($user));
+    $bot->reply(print_r($user->getInfo()));
 });
 
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
