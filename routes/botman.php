63c148abe687eeccdbe709357b799556ebb9589b
Alexander Park <parkalexser@gmail.com>
2021-05-05 17:25:32 +0500

    print_r to true

--- routes/botman.php
+++ routes/botman.php
@@ -7,7 +7,7 @@ $botman->hears('/random', 'App\Http\Controllers\AllBreedsController@random');
 
 $botman->hears('Hi', function ($bot) {
 	$user = $bot->getUser();
-    $bot->reply(print_r($user->getInfo()));
+    $bot->reply(print_r($user->getInfo(), true));
 });
 
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
